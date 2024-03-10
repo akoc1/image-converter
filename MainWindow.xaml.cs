b640c1a167fcb6cdf412a8f01ca52edf7c860a53
akoc1 <72667213+akoc1@users.noreply.github.com>
2024-03-10 22:43:54 +0300

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,132 @@
+﻿using System.Windows;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Media.Imaging;
+using Microsoft.Win32;
+
+namespace ImageConverter
+{
+    public partial class MainWindow : Window
+    {
+        enum ConvertTypes
+        {
+            PNGtoJPG = 0,
+            JPGtoPNG = 1
+        }
+
+        ConvertTypes convertType = ConvertTypes.PNGtoJPG;
+
+        private string? selectedFile;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ComboBox.SelectedIndex = 0;
+        }
+
+        private void Window_Drop(object sender, DragEventArgs e)
+        {
+            string[] filePath = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+            selectedFile = filePath[0];
+
+            SetPreviewImage();
+        }
+
+        private void SetPreviewImage()
+        {
+            if (!File.Exists(selectedFile))
+                return;
+
+            BitmapImage bitmap = new BitmapImage();
+            bitmap.BeginInit();
+            bitmap.UriSource = new Uri(selectedFile);
+            bitmap.EndInit();
+
+            ImageViewer.Source = bitmap;
+        }
+
+        private void ConvertButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!File.Exists(selectedFile))
+            {
+                MessageBox.Show("File not selected.", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+                return;
+            }
+
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+            switch (convertType)
+            {
+                case ConvertTypes.PNGtoJPG:
+                    Image png = Image.FromFile(selectedFile);
+
+                    png.Save(desktopPath + @"/" + Path.GetFileNameWithoutExtension(selectedFile) + ".jpeg", ImageFormat.Jpeg);
+                    png.Dispose();
+
+                    break;
+                case ConvertTypes.JPGtoPNG:
+                    Image jpg = Image.FromFile(selectedFile);
+                    
+                    jpg.Save(desktopPath + @"/" + Path.GetFileNameWithoutExtension(selectedFile) + ".png", ImageFormat.Png);
+                    jpg.Dispose();
+                    
+                    break;
+            }
+
+            InfoTextBlock.Text = "File saved to desktop!";
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            ImageViewer.Source = null;
+            selectedFile = null;
+            InfoTextBlock.Text = "Select a file to convert";
+        }
+
+        private void ImageViewer_SourceUpdated(object sender, System.Windows.Data.DataTransferEventArgs e)
+        {
+            if (ImageViewer.Source == null)
+            {
+                
+            }
+        }
+
+        private void TextBlock_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+
+            dialog.Title = "Select file";
+
+            switch (convertType)
+            {
+                case ConvertTypes.PNGtoJPG:
+                    dialog.Filter = "Image file(*.png)|*.png";
+                    break;
+                case ConvertTypes.JPGtoPNG:
+                    dialog.Filter = "Image file(*.jpg;*.jpeg)|*.jpg;*.jpeg";
+                    break;
+            }
+
+            Nullable<bool> result = dialog.ShowDialog(this);
+
+            if (result == true)
+            {
+                selectedFile = dialog.FileName;
+
+                InfoTextBlock.Text = Path.GetFileNameWithoutExtension(selectedFile);
+                SetPreviewImage();
+            }
+        }
+
+        private void ComboBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            if (ComboBox.SelectedIndex == 0)
+                convertType = ConvertTypes.PNGtoJPG;
+            else
+                convertType = ConvertTypes.JPGtoPNG;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# image-converter
+ Basic image converter made in C# WPF (Windows Presentation Foundation) .NET 8
